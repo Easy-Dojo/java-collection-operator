69320584369758b9e1f8a8786dfbf4e414065614
llr392425595 <392425595@qq.com>
2018-04-28 13:59:47 +0800

    重构功能函数isEven，重构getListByInterval方法

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,82 +2,98 @@ package com.thoughtworks.collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CollectionOperator {
-    private boolean isEven(int num) {
-        return num % 2==0 ? true : false;
-    }
-    private List<Integer> getEvensArray(List<Integer> arr) {
-        List<Integer> result = new ArrayList<>();
-        for (int num : arr) {
-            if (isEven(num)) {
-                result.add(num);
-            }
-        }
-        return result;
-    }
-    private boolean isContainedIntArray(int num, int[] arr) {
-        List list = new ArrayList<String>();
-        for(int i=0;i<arr.length;i++){
-            list.add(arr[i]);
-        }
-        return list.contains(num);
-    }
-    private boolean isContainedIntegerArray(Integer num, Integer[] arr) {
-        List list = new ArrayList<String>();
-        for(int i=0;i<arr.length;i++){
-            list.add(arr[i]);
-        }
-        return list.contains(num);
-    }
 
-    public List<Integer> getListByInterval(int left, int right) {
-        List<Integer> result = new ArrayList<>();
-        if (left < right) {
-            for (int i = left; i <= right; i++) {
-                result.add(i);
-            }
-        }else {
-            for (int i = left; i >= right; i--) {
-                result.add(i);
-            }
-        }
-        return result;
+  private boolean isEven(int num) {
+    return num % 2 == 0;
+  }
+
+  private List<Integer> getEvensArray(List<Integer> arr) {
+    List<Integer> result = new ArrayList<>();
+    for (int num : arr) {
+      if (isEven(num)) {
+        result.add(num);
+      }
     }
-    public List<Integer> getEvenListByIntervals(int left, int right) {
-        List<Integer> listByInterval = getListByInterval(left, right);
-        return getEvensArray(listByInterval);
+    return result;
+  }
+
+  private boolean isContainedIntArray(int num, int[] arr) {
+    List list = new ArrayList<String>();
+    for (int i = 0; i < arr.length; i++) {
+      list.add(arr[i]);
     }
-    public List<Integer> popEvenElments(int[] array) {
-        List<Integer> result = new ArrayList<>();
-        for (int num : array) {
-            if (isEven(num)) {
-                result.add(num);
-            }
-        }
-        return result;
+    return list.contains(num);
+  }
+
+  private boolean isContainedIntegerArray(Integer num, Integer[] arr) {
+    List list = new ArrayList<String>();
+    for (int i = 0; i < arr.length; i++) {
+      list.add(arr[i]);
     }
-    public int popLastElment(int[] array) {
-        int index = array.length-1;
-        return array[index];
+    return list.contains(num);
+  }
+
+  //  选出给定区间中所有的数字
+  public List<Integer> getListByInterval(int left, int right) {
+    Stream<Integer> integerStream = IntStream
+        .rangeClosed(smallerOf(left, right), biggerOf(left, right)).boxed();
+    return left < right ? integerStream.collect(Collectors.toList())
+        : integerStream.sorted(Comparator.reverseOrder())
+            .collect(Collectors.toList());
+  }
+
+  private Integer biggerOf(int left, int right) {
+    return right > left ? right : left;
+  }
+
+  private Integer smallerOf(int left, int right) {
+    return left < right ? left : right;
+  }
+
+  public List<Integer> getEvenListByIntervals(int left, int right) {
+    List<Integer> listByInterval = getListByInterval(left, right);
+    return getEvensArray(listByInterval);
+  }
+
+  public List<Integer> popEvenElments(int[] array) {
+    List<Integer> result = new ArrayList<>();
+    for (int num : array) {
+      if (isEven(num)) {
+        result.add(num);
+      }
     }
-    public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        List<Integer> result = new ArrayList<>();
-        for (int num : firstArray) {
-            if(isContainedIntArray(num,secondArray)){
-                result.add(num);
-            }
-        }
-        return result;
+    return result;
+  }
+
+  public int popLastElment(int[] array) {
+    int index = array.length - 1;
+    return array[index];
+  }
+
+  public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
+    List<Integer> result = new ArrayList<>();
+    for (int num : firstArray) {
+      if (isContainedIntArray(num, secondArray)) {
+        result.add(num);
+      }
     }
-    public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        List<Integer> result = new ArrayList<>(Arrays.asList(firstArray));
-        for (int num : secondArray) {
-            if (!isContainedIntegerArray(num,firstArray)) {
-                result.add(num);
-            }
-        }
-        return result;
+    return result;
+  }
+
+  public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
+    List<Integer> result = new ArrayList<>(Arrays.asList(firstArray));
+    for (int num : secondArray) {
+      if (!isContainedIntegerArray(num, firstArray)) {
+        result.add(num);
+      }
     }
+    return result;
+  }
 }
