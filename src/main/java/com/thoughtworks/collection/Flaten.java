6231b362d281627e10d13d327529d28c301a1dc4
llr392425595 <392425595@qq.com>
2018-04-28 15:18:35 +0800

    重构 Flaten：二维数组变一维

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,24 +1,18 @@
 package com.thoughtworks.collection;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Flaten {
 
     Integer[][] array;
-    public Flaten(Integer[][] array) {
+    Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        List<Integer> result = new ArrayList<>();
-        for (Integer[] integers : array) {
-            for (Integer integer : integers) {
-                result.add(integer);
-            }
-        }
-        return result;
+      return Arrays.stream(array).flatMap(Arrays::stream).collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
